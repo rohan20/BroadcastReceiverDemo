a9a90f4da01e1fca954a18b053f0a3d0fae08841
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-08-08 12:03:39 +0530

    Receiver added

---
+++ app/src/main/java/MyBroadcastReceiver.java
@@ -0,0 +1,15 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by rohantaneja on 08/08/17.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
